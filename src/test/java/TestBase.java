cd8d9cbb66f08c9057b2f811576431ebf3380eaa
Evgeniia Miroshina <evgeniia.miroshina@t-systems.com>
2022-01-27 16:36:36 +0300

    updated build.gradle

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -15,7 +15,7 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "2560x1440";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
