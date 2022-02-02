f7afee48d9e6933dfeb9e8d99ac1fd0e13ec0584
Evgeniia Miroshina <evgeniia.miroshina@t-systems.com>
2022-02-02 16:57:02 +0300

    test changed

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -15,26 +15,35 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        String browser = System.getProperty("browser", "chrome1");
-        String version = System.getProperty("version", "911");
-        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub1");
-        String login = System.getProperty("login", "user11");
-        String pass = System.getProperty("pass", "12341");
+        String login = System.getProperty("login");
+        String password = System.getProperty("password");
+        String url = System.getProperty("url");
+        String browser = System.getProperty("browser");
+        String browserVersion = System.getProperty("version");
+        String browserSize = System.getProperty("browserSize");
+        String remoteUrl = "https://" + login + ":" + password + "@" + url;
 
-        String url = "https://" + login + ":" + pass + "@" + remoteUrl;
-        Configuration.remote = url;
-
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion; // available versions: chrome - 90, 91 opera - 76, 77 firefox - 88, 89
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "2560x1440";
+        Configuration.browserSize = browserSize;
+        Configuration.remote = remoteUrl;
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+    }
 
-        Attach.attachAsText("Browser: ", browser);
-        Attach.attachAsText("Version: ", version);
-        Attach.attachAsText("Remote Url: ", remoteUrl);
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "2560x1440";
+
+
+
+       // Attach.attachAsText("Browser: ", browser);
+        //Attach.attachAsText("Version: ", version);
+       // Attach.attachAsText("Remote Url: ", remoteUrl);
     }
 
     @AfterEach
